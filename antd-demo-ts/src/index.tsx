54db786147ed001ea5c19b42fdba2102e21eb67a
qing zou <qing.zou@sail-fs.com>
2018-06-27 18:11:57 +0800

    update

--- antd-demo-ts/src/index.tsx
+++ antd-demo-ts/src/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
+import SchoolName from './firstTest';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />,
+  // <App />,
+  <SchoolName/>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
